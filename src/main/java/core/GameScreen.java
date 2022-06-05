c9069051d86ea969f8e062e08f448b8fa40eda05
ivmov <ivmovchanets@gmail.com>
2022-06-05 15:45:16 +0300

    add random square painted)

--- src/main/java/core/GameScreen.java
+++ src/main/java/core/GameScreen.java
@@ -2,16 +2,32 @@ package core;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GameScreen extends JPanel {
 
+    private Random random;
+
     public GameScreen() {
+        random = new Random();
     }
-    public void paintComponent(Graphics g){
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.red);
-        g.fillRect(50, 50, 100, 100);
 
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                g.setColor(getRandomColor());
+                g.fillRect(x * 32, y*32, 32, 32);
+            }
+        }
+    }
+
+    private Color getRandomColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return new Color(r, g, b);
     }
 }
