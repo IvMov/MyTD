5ccaba0381374561c8f0dd10cf4d6eb3389039bd
ivmov <ivmovchanets@gmail.com>
2022-06-05 16:11:48 +0300

    set all sprites in sprite list, get ork from spriteList

--- src/main/java/core/GameScreen.java
+++ src/main/java/core/GameScreen.java
@@ -3,6 +3,7 @@ package core;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScreen extends JPanel {
@@ -10,24 +11,36 @@ public class GameScreen extends JPanel {
     private Random random;
     private BufferedImage img;
 
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
+
     public GameScreen(BufferedImage img) {
         this.img = img;
         random = new Random();
+        loadSprites();
+    }
+
+    private void loadSprites() {
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                BufferedImage subImage = img.getSubimage(x * 32, y * 32, 32, 32);
+                sprites.add(subImage);
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        BufferedImage orkImg = img.getSubimage(9*32, 32, 32,32);
-        g.drawImage(orkImg, 0, 0 , null);
 
 
 //        for (int y = 0; y < 20; y++) {
 //            for (int x = 0; x < 20; x++) {
-//                g.setColor(getRandomColor());
-//                g.fillRect(x * 32, y*32, 32, 32);
+//
+//
 //            }
 //        }
     }
+
+
 
     private Color getRandomColor() {
         int r = random.nextInt(256);
