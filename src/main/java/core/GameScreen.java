26e202272cabf73b25d5798e9e02b790b1f9f684
ivmov <ivmovchanets@gmail.com>
2022-06-06 09:48:54 +0300

    add FPS counter

--- src/main/java/core/GameScreen.java
+++ src/main/java/core/GameScreen.java
@@ -10,6 +10,8 @@ public class GameScreen extends JPanel {
 
     private Random random;
     private BufferedImage img;
+    private long lastTime;
+    private int frames;
 
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
@@ -37,6 +39,14 @@ public class GameScreen extends JPanel {
                 g.drawImage(sprites.get(getRandomInt()), x * 32, y * 32, null);
             }
         }
+
+        frames++;
+        if (System.currentTimeMillis() - lastTime >= 1000) {
+            System.out.println("FPS: " + frames);
+            frames = 0;
+            lastTime = System.currentTimeMillis();
+        }
+        repaint();
     }
 
     private int getRandomInt() {
